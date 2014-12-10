b1e785a0c5720ca68a89d4cecc3fbb0e467dc8cd
gameboy1024 <gameboy1024@hotmail.com>
2014-12-10 10:39:53 +0100

    Proto for LexicalAnalyser

--- _build/Main/Compile.ml
+++ _build/Main/Compile.ml
@@ -1,3 +1,5 @@
 (* verbose is a boolean that you can use to switch to a verbose output (for example, to dump all the ast) *)
 let execute lexbuf verbose = 
-  print_endline "parsing todo"
+  print_endline "parsing todo";
+  print_endline("verbose=" ^ string_of_bool(verbose));
+  LexicalAnalyser
